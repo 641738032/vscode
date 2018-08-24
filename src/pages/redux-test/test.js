0fe6ca10375be0cf901c90171efff0f4349ba7af
zhuxiaolin <641738032@qq.com>
2018-08-24 16:54:58 +0800

    test新增方法

--- src/pages/redux-test/test.js
+++ src/pages/redux-test/test.js
@@ -2,6 +2,38 @@ import React from 'react';
 import './test.less';
 
 class Test extends React.component{
+    constructor(){
 
+    } 
+    getChildContext(){
+
+    }
+    componentWillMount(){
+
+    }
+    componentDidMount(){
+
+    }
+    componentWillReceiveProps(){
+
+    }
+    shouldComponentUpdate(){
+
+    }
+    componentWillUpdate(){
+
+    }
+    componentDidUpdate(){
+
+    }
+    componentWillUnmount(){
+
+    }
+    OnClickSubmit=()=>{
+
+    }
+    render(){
+        
+    }
 }
 export default Test;
